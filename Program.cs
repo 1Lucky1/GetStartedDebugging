69c1b0abf058aafec3312a30cfba1f4c54de53dd
Dark Hacker <rukapop1000@gmail.com>
2024-09-11 10:30:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GetStartedDebugging;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char[] letters = { 'g', 'a', 'p', 'u', ' ', 'z', 'm', 'k' };
+        string name = "";
+        int[] a = new int[10];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+        static void SendMessage(string name, int msg)
+        {
+            Console.WriteLine("Hello, " + name + "! Count to " + msg);
+        }
+    }
+}
